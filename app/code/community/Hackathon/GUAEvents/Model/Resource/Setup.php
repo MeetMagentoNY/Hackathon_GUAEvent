433c427036dc4e358b9d312c1629fb96a112d086
Joseph Leedy <joseph@leedy.us>
2014-09-21 16:14:26 -0400

    Created setup model and setup script

---
+++ app/code/community/Hackathon/GUAEvents/Model/Resource/Setup.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Google Universal Analytics Events
+ *
+ * @category Hackathon
+ * @package Hackathon_GUAEvents
+ * @license http://opensource.org/licenses/OSL-3.0 OSL-3.0
+ */
+
+/**
+ * Setup Model Resource Class
+ *
+ * @category Hackathon
+ * @package Hackathon_GUAEvents
+ * @author Joseph Leedy <joseph@leedy.us>
+ */
+class Hackathon_GUAEvents_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
+{
+}
---
+++ app/code/community/Hackathon/GUAEvents/sql/hackathon_guaevents_setup/install-0.1.0.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Google Universal Analytics Events
+ *
+ * @category Hackathon
+ * @package Hackathon_GUAEvents
+ * @license http://opensource.org/licenses/OSL-3.0 OSL-3.0
+ */
+
+/** @var $installer Hackathon_GUAEvents_Model_Resource_Setup */
+$installer = $this;
+
+$connection = $installer->getConnection();
+
+$installer->startSetup();
+
+$eventTable = $connection->newTable($installer->getTable('hackathon_guaevents/event'))
+    ->addColumn(
+        'event_id',
+        Varien_Db_Ddl_Table::TYPE_INTEGER,
+        null,
+        array(
+            'identity' => true,
+            'unsigned' => true,
+            'nullable' => false,
+            'primary'  => true,
+        ),
+        'Unique identifier for the event'
+    )
+    ->addColumn(
+        'trigger_type',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        255,
+        array(
+            'nullable' => false,
+        ),
+        'The type of event trigger'
+    )
+    ->addColumn(
+        'trigger_data',
+        Varien_Db_Ddl_Table::TYPE_TEXT,
+        null,
+        array(
+            'nullable' => false,
+        ),
+        'Serialized array containing the data for the trigger'
+    )
+    ->addColumn(
+        'event_category',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        255,
+        array(),
+        'The event\'s category'
+    )
+    ->addColumn(
+        'event_label',
+        Varien_Db_Ddl_Table::TYPE_VARCHAR,
+        255,
+        array(
+            'nullable' => false,
+        ),
+        'The event\'s label'
+    )
+    ->addColumn(
+        'event_value',
+        Varien_Db_Ddl_Table::TYPE_INTEGER,
+        null,
+        array(
+            'unsigned' => true,
+            'nullable' => false,
+        ),
+        'The event\'s value'
+    )
+    ->setComment('Google Universal Analytics Events table');
+
+$connection->createTable($eventTable);
+
+$installer->endSetup();
