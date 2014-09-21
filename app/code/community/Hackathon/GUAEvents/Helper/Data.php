e3d05eab4ccdd17856aca605475eea7ad115abb1
David Robinson <david.robinson@aoemedia.com>
2014-09-21 15:01:56 -0700

    Added menu item and acl

---
+++ app/code/community/Hackathon/GUAEvents/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * GUAEvent Helper
+ *
+ * @author David Robinson <david.robinson@aoe.com>
+ * @since 2014-09-21
+ */
+
+class Hackathon_GUAEvents_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
